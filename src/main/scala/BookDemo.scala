8b1f4a57499bfd32384b5845dd042826969d9311
emilytozier <emilytozier@yandex.ru>
2024-10-23 15:04:13 +0400

    extra commit

--- src/main/scala/BookDemo.scala
+++ src/main/scala/BookDemo.scala
@@ -1,5 +1,11 @@
+//Создайте класс "Книга" с полями "заголовок", "автор" и "год издания". // далее все что в "" - переводить на англ, тут на русском для облегчения восприятия
+//Переопределите в нем метод toString.
+//Создайте companion object для класса "Книга", в котором будет метод createBook, принимающий "название", "автора" и "год выпуска", и возвращающий объект Book с этими значениями. Создайте таким образом экземпляр Book и выведите его в консоль.
 object BookDemo {
-  class Book(val name: String, val author: String, val year: Int)
+  class Book(val name: String, val author: String, val year: Int) {
+    override def toString: String = s"(name $name, author $author, year $year)"
+  }
+  
   object Book {
     def createBook(name: String, author: String, year: Int): Book = new Book(name, author, year)
     def infoBook(name: String, author: String, year: Int): String = {
